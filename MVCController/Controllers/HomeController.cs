f9ffb1f55cc409ade85dbb3e8f2cd5160eb40ec5
Admin <Admin@DESKTOP-KB0VR9M>
2024-03-28 19:42:01 +0530

    Add project files.

---
+++ MVCController/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+
+namespace MVCController.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //public string Greet()
+        //{
+        //    return "Welcome to MVC5";
+        //}
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Aboutus()
+        {
+            return View();
+        }
+        public ActionResult Contactus()
+        {
+            return View();
+        }
+        public ActionResult Blogs()
+        {
+            return View();
+        }
+    }
+}
