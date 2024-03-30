ec8a7b678d87c029e00fea16cf31c9596497ead1
iprasadreddy <irigalaprasadreddy@gmail.com>
2024-03-30 17:42:56 +0530

    VIEWS in ASP.Net MVC

---
+++ MVCVIEWprogramming/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCVIEWprogramming.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Index1()
+        {
+            return View();
+        }
+        public ActionResult Index2()
+        {
+            return View();
+        }
+          
+        public ActionResult Index3()
+        {
+            return View();
+        }
+        public ActionResult Index4()
+        {
+            return View();
+        }
+        public ActionResult Index5()
+        {
+            List<string> list = new List<string>() { "Admin", "Trainer", "HR", "Student" };
+            SelectList items = new SelectList(list);
+            ViewData["logins"] = items;
+            return View();
+        }
+    }
+}
